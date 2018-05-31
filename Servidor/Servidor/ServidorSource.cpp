57318830da866ca5fadbbbf24be902e7d60b8c67
LipaGitHub <lipapsousa@hotmail.com>
2018-05-31 20:12:54 +0100

    kvjbsdpifbç

--- Servidor/Servidor/ServidorSource.cpp
+++ Servidor/Servidor/ServidorSource.cpp
@@ -8,20 +8,22 @@
 #include <io.h>
 #include "..\..\DLL\DLL\Estruturas.h"
 
-
-
 DWORD WINAPI gerarInimigas();
 void navesInimigas(NaveInvasora *naveInvasora);
 void criarMapa();
-void imprimeMapa();
 void dadosIniciais();
 void criaNavesInimigas(int nCriar);
+int IniciarMemoriaMutexSemaforo();
+DWORD WINAPI leMsg();
 
+DadosJogo *mPartilhadaDadosJogo;
+ZonaMsg *mPartilhadaZonaMsg;
 Celula *Tiros;
 
+HANDLE PodeEscrever, PodeLer, hMutex;
+TCHAR NomeSemaforoPodeEscrever[] = TEXT("Semáforo Pode Escrever"), NomeSemaforoPodeLer[] = TEXT("Semáforo Pode Ler");
 
 DWORD *threadId;
-
 
 
 int _tmain(int argc, LPTSTR argv[]) {
@@ -30,18 +32,10 @@ int _tmain(int argc, LPTSTR argv[]) {
 	_setmode(_fileno(stdin), _O_WTEXT);
 	_setmode(_fileno(stdout), _O_WTEXT);
 #endif
-
-	DadosJogo dJogo;
-
-	HANDLE hMap;
-	LARGE_INTEGER t;
-
-	//DWORD *mapa;
-
-	t.QuadPart = sizeof(DadosJogo);
-	hMap = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, t.HighPart, t.LowPart, TEXT("shm"));
-
-	mPartilhada = (DadosJogo*)MapViewOfFile(hMap, FILE_MAP_ALL_ACCESS, 0, 0, (SIZE_T)t.QuadPart);
+	
+	if (IniciarMemoriaMutexSemaforo() == (-1)) {
+		return -1;
+	}
 
 	_tprintf(TEXT("-------------------------------------\n"));
 	_tprintf(TEXT("----BEM-VINDO PHOENIX MULTIPLAYER----\n"));
@@ -49,13 +43,47 @@ int _tmain(int argc, LPTSTR argv[]) {
 
 	dadosIniciais();
 
+	//WaitForSingleObject(PodeLer, INFINITE);
 	criarMapa();
-	criaNavesInimigas(mPartilhada->dConfiguraveis.nInimigas);
-	
-	HANDLE imprimeMap = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)imprimeMapa, NULL, 0, NULL);
-	gerarInimigas();
-	
+	criaNavesInimigas(mPartilhadaDadosJogo->dConfiguraveis.nInimigas);
+
+	ReleaseSemaphore(PodeLer, 1, NULL);
+
+	//HANDLE imprimeMap = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)imprimeMapa, mPartilhadaDadosJogo, 0, NULL);
+	HANDLE lerBuffer = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)leMsg, NULL, 0, NULL);
+
+	//gerarInimigas();
+
 	system("pause");
+}
+
+int IniciarMemoriaMutexSemaforo() {
+	HANDLE hDadosJogo, hZonaMsg;
+	LARGE_INTEGER t, d;
+
+	hMutex = CreateMutex(NULL, FALSE, TEXT("Mutex Out"));
+	PodeEscrever = CreateSemaphore(NULL, 0, Buffers, NomeSemaforoPodeEscrever);
+	PodeLer = CreateSemaphore(NULL, 0, Buffers, NomeSemaforoPodeLer);
+
+	t.QuadPart = sizeof(DadosJogo);
+	d.QuadPart = sizeof(ZonaMsg);
+	hDadosJogo = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, t.HighPart, t.LowPart, TEXT("Memoria Partilhada Dado Jogo"));
+	hZonaMsg = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, d.HighPart, d.LowPart, TEXT("Memoria Partilhada MSG"));
+
+	if (PodeEscrever == NULL || PodeLer == NULL) {
+		_tprintf(TEXT("[Erro]Criação de objectos do Windows(%d)\n"), GetLastError());
+		return -1;
+	}
+
+	mPartilhadaDadosJogo = (DadosJogo*)MapViewOfFile(hDadosJogo, FILE_MAP_ALL_ACCESS, 0, 0, (SIZE_T)t.QuadPart);
+	mPartilhadaZonaMsg = (ZonaMsg*)MapViewOfFile(hZonaMsg, FILE_MAP_ALL_ACCESS, 0, 0, (SIZE_T)d.QuadPart);
+
+	if (mPartilhadaDadosJogo == NULL && mPartilhadaZonaMsg == NULL) {
+		_tprintf(TEXT("[Erro]Mapeamento da memória partilhada(%d)\n"), GetLastError());
+		return -1;
+	}
+	
+	return 0;
 }
 
 
@@ -63,15 +91,15 @@ void dadosIniciais() {
 	_tprintf(TEXT("\n\nDados Configuráveis:\n"));
 	_tprintf(TEXT("\n-------------------------------------\n"));
 	_tprintf(TEXT("Quantas Naves Inimigas? "));
-	_tscanf_s(TEXT("%d"), &(mPartilhada->dConfiguraveis.nInimigas));
+	_tscanf_s(TEXT("%d"), &(mPartilhadaDadosJogo->dConfiguraveis.nInimigas));
 	_tprintf(TEXT("Quantos Powerups? "));
-	_tscanf_s(TEXT("%d"), &(mPartilhada->dConfiguraveis.nPowerups));
+	_tscanf_s(TEXT("%d"), &(mPartilhadaDadosJogo->dConfiguraveis.nPowerups));
 	_tprintf(TEXT("Tempo de Duração? "));
-	_tscanf_s(TEXT("%d"), &(mPartilhada->dConfiguraveis.nDuracao));
+	_tscanf_s(TEXT("%d"), &(mPartilhadaDadosJogo->dConfiguraveis.nDuracao));
 	_tprintf(TEXT("Probabilidade de Ocorrência de Powerups? "));
-	_tscanf_s(TEXT("%d"), &(mPartilhada->dConfiguraveis.nProb));
+	_tscanf_s(TEXT("%d"), &(mPartilhadaDadosJogo->dConfiguraveis.nProb));
 	_tprintf(TEXT("Quantas vidas iniciais? "));
-	_tscanf_s(TEXT("%d"), &(mPartilhada->dConfiguraveis.nVidasIni));
+	_tscanf_s(TEXT("%d"), &(mPartilhadaDadosJogo->dConfiguraveis.nVidasIni));
 }
 
 
@@ -83,29 +111,29 @@ DWORD WINAPI gerarInimigas() {
 	HANDLE *tIni;
 	HANDLE hMutex;
 
-	tIni = (HANDLE*)malloc(mPartilhada->dConfiguraveis.nInimigas * sizeof(HANDLE));		//alocação de memória para o n° de naves invasoras
-	threadId = (DWORD *)malloc(mPartilhada->dConfiguraveis.nInimigas * sizeof(DWORD));	//alocação de memória para os ID's de cada thread de nave invasora
+	tIni = (HANDLE*)malloc(mPartilhadaDadosJogo->dConfiguraveis.nInimigas * sizeof(HANDLE));		//alocação de memória para o n° de naves invasoras
+	threadId = (DWORD *)malloc(mPartilhadaDadosJogo->dConfiguraveis.nInimigas * sizeof(DWORD));	//alocação de memória para os ID's de cada thread de nave invasora
 	hMutex = CreateMutex(NULL, FALSE, NULL);
 	if (hMutex == NULL) {
 		_tprintf(TEXT("Deu erro no mutex..."));
 	}
 
-	for (int i = 0; i < mPartilhada->dConfiguraveis.nInimigas; i++) {
+	for (int i = 0; i < mPartilhadaDadosJogo->dConfiguraveis.nInimigas; i++) {
 		WaitForSingleObject(hMutex, INFINITE);
-		tIni[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)navesInimigas, &(mPartilhada->nInimigas[i]), 0, &threadId[i]);
+		tIni[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)navesInimigas, &(mPartilhadaDadosJogo->nInimigas[i]), 0, &threadId[i]);
 		ReleaseMutex(hMutex);
 	}
 
-	WaitForMultipleObjects(mPartilhada->dConfiguraveis.nInimigas, tIni, TRUE, INFINITE);
+	WaitForMultipleObjects(mPartilhadaDadosJogo->dConfiguraveis.nInimigas, tIni, TRUE, INFINITE);
 	_tprintf(TEXT("[Thread Principal %d]Vou terminar..."), GetCurrentThreadId());
 	return 0;
 }
 
 void criaNavesInimigas(int nCriar) {
 	for (int i = 0; i < nCriar; i++) {
-		mPartilhada->nInimigas[i].Posicao.caracter = 'I';
-		mPartilhada->nInimigas[i].Posicao.x = 8 + i;
-		mPartilhada->nInimigas[i].Posicao.y = 1;
+		mPartilhadaDadosJogo->nInimigas[i].Posicao.caracter = 'I';
+		mPartilhadaDadosJogo->nInimigas[i].Posicao.x = 8 + i;
+		mPartilhadaDadosJogo->nInimigas[i].Posicao.y = 1;
 	}
 	_tprintf(TEXT("\n--- Naves invasoras criadas ---\n"));
 	system("pause");
@@ -117,13 +145,14 @@ void navesInimigas(NaveInvasora *naveInvasora) {
 	//_tprintf(TEXT("[Thread Nave inimiga %d]\n"), GetCurrentThreadId());
 
 	while (1) {
+		//		[ATENÇAO]
 		//QUANDO ESCREVE OU LE PROTEJER Memoria partilhada
-		mPartilhada->Mapa[naveInvasora->Posicao.x][naveInvasora->Posicao.y].caracter = ' ';
+		mPartilhadaDadosJogo->Mapa[naveInvasora->Posicao.x][naveInvasora->Posicao.y].caracter = ' ';
 
 		if (((naveInvasora->Posicao.y % 2) != 0) && naveInvasora->Posicao.x < 18) {
 			naveInvasora->Posicao.x++;
 		}
-		else if(((naveInvasora->Posicao.y % 2) != 0) && naveInvasora->Posicao.x == 18) {
+		else if (((naveInvasora->Posicao.y % 2) != 0) && naveInvasora->Posicao.x == 18) {
 			naveInvasora->Posicao.y++;
 			naveInvasora->Posicao.x--;
 		}
@@ -133,7 +162,7 @@ void navesInimigas(NaveInvasora *naveInvasora) {
 				naveInvasora->Posicao.y++;
 			}
 		}
-		else if(((naveInvasora->Posicao.y % 2) != 0) && naveInvasora->Posicao.x == 1){
+		else if (((naveInvasora->Posicao.y % 2) != 0) && naveInvasora->Posicao.x == 1) {
 			naveInvasora->Posicao.y++;
 			naveInvasora->Posicao.x--;
 		}
@@ -148,37 +177,32 @@ void criarMapa() {
 	for (int i = 0; i < L; i++) {
 		for (int j = 0; j < C; j++) {
 			if (i == 0 || i == (L - 1)) {
-				mPartilhada->Mapa[j][i].caracter = '-';
+				mPartilhadaDadosJogo->Mapa[j][i].caracter = '-';
 			}
 			else if (j == 0 || j == (C - 1)) {
-				mPartilhada->Mapa[j][i].caracter = '|';
+				mPartilhadaDadosJogo->Mapa[j][i].caracter = '|';
 			}
 			else {
-				mPartilhada->Mapa[j][i].caracter = ' ';
+				mPartilhadaDadosJogo->Mapa[j][i].caracter = ' ';
 			}
 		}
 	}
 }
 
-void imprimeMapa() {
-	while (1) {
-		system("cls");
-		for (int i = 0; i < mPartilhada->dConfiguraveis.nInimigas; i++) {
-			mPartilhada->Mapa[mPartilhada->nInimigas[i].Posicao.x][mPartilhada->nInimigas[i].Posicao.y].caracter = mPartilhada->nInimigas->Posicao.caracter;
-			//_tprintf(TEXT("%c"), mPartilhada->Mapa[mPartilhada->nInimigas[i].Posicao.x][mPartilhada->nInimigas[i].Posicao.y].caracter);
-		}
 
-		for (int i = 0; i < L; i++) {
-			for (int j = 0; j < C; j++) {
-				if (mPartilhada->Mapa[j][i].caracter != ' ') {
-					_tprintf(TEXT("%c"), mPartilhada->Mapa[j][i].caracter);
-				}
-				else {
-					_tprintf(TEXT(" "));
-				}
-			}
-			_tprintf(TEXT("\n"));
-		}
-		Sleep(1000);
+DWORD WINAPI leMsg() {
+
+	int pos;
+	while(1)
+	{
+		WaitForSingleObject(PodeLer, INFINITE);
+		WaitForSingleObject(hMutex, INFINITE);
+		pos = mPartilhadaZonaMsg->out;
+		mPartilhadaZonaMsg->out = (mPartilhadaZonaMsg->out + 1) % Buffers;
+		ReleaseMutex(hMutex);
+		_tprintf(TEXT("Ler do Buffer %d o valor '%s'\n"), pos, mPartilhadaZonaMsg->buf[pos]); // Reader reads data
+		ReleaseSemaphore(PodeEscrever, 1, NULL);
 	}
+
 }
+
